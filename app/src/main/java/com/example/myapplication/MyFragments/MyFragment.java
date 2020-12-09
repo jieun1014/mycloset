0865751ed97ea75453a7f50b8d360bf81401595f
taiya1014@naver.com <58425842yh>
2020-12-09 11:27:21 +0900

    내 정보 페이지 수정 및 닉네임 수정 기능 추가

--- app/src/main/java/com/example/myapplication/MyFragments/MyFragment.java
+++ app/src/main/java/com/example/myapplication/MyFragments/MyFragment.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,12 +23,25 @@ import com.example.myapplication.BoardFragments.BoardWriteFragment;
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.MainFragments.Login;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MyFragment extends Fragment {
     private MainActivity activity;
 
     private ImageButton myPostBtn, myCommentBtn, myInfoBtn, logoutBtn;
+    private TextView nickname;
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private String Nickname;
 
     @Override
     public void onAttach(Context context) {
@@ -48,9 +63,26 @@ public class MyFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_my, container, false);
 
+        nickname = (TextView) root.findViewById(R.id.nickNameText);
         myPostBtn = root.findViewById(R.id.myPostBtn);
         myInfoBtn = root.findViewById(R.id.myInfoBtn);
         logoutBtn = root.findViewById(R.id.logoutBtn);
+
+        findUserNickname();
+
+        nickname.setText(Nickname);
+        Log.d("Nickname test ","the Nickname is " + Nickname);
+
+        myInfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyInfoFragment myInfoFragment = new MyInfoFragment();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, myInfoFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
 
         myPostBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,5 +120,31 @@ public class MyFragment extends Fragment {
         });
 
         return root;
+    }
+
+    private void findUserNickname () {
+        db.collection("UserInfo")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (document.getData().get("uid").toString().equals(user.getUid())) {
+                                    Nickname = document.getData().get("nickname").toString();
+                                }
+                            }
+                        } else {
+                            Nickname = "익명";
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Nickname = "익명";
+                        Log.w("Nickname Load Error", e);
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/myapplication/MyFragments/MyInfoFragment.java
+++ app/src/main/java/com/example/myapplication/MyFragments/MyInfoFragment.java
@@ -1,22 +1,47 @@
 package com.example.myapplication.MyFragments;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.MainActivity;
+import com.example.myapplication.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class MyInfoFragment extends Fragment {
-    private FirebaseAuth firebaseAuth;
-    private FirebaseFirestore firebaseFirestore;
-
     MainActivity activity;
+
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private EditText nickname;
+    private TextView email;
+    private Button editText_nickname_btn, modeify_email_btn, modeify_password_btn;
+
+    private String Nickname = "익명", Email ="이메일", Uid, Did;
+
 
     @Override
     public void onAttach(Context context) {
@@ -28,5 +53,94 @@ public class MyInfoFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         activity = null;
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_my_information, container, false);
+
+        nickname = (EditText) root.findViewById(R.id.editText_nickname);
+        email = (TextView) root.findViewById(R.id.editText_email);
+
+        editText_nickname_btn = (Button) root.findViewById(R.id.editText_nickname_btn);
+
+        modeify_email_btn = (Button) root.findViewById(R.id.modeify_Email_btn);
+        modeify_password_btn = (Button) root.findViewById(R.id.create_New_Password_btn);
+
+        getUserInfo();
+
+        editText_nickname_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!nickname.getText().toString().isEmpty() && nickname.getText().toString().length() <= 6) {
+                    DocumentReference editUserNickname = firebaseFirestore.collection("UserInfo").document(Did);
+                    editUserNickname
+                            .update("nickname", nickname.getText().toString())
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("Nickname Edit Result", "DocumentSnapshot successfully updated!");
+                                    Toast.makeText(getContext(), "닉네임이 변경되었습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("Nickname Edit Result", "Error updating document", e);
+                                    Toast.makeText(getContext(), "닉네임 변경에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                } else {
+                    Toast.makeText(getContext(), "닉네임 설정 규칙을 준수하세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        modeify_email_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        modeify_password_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return root;
+    }
+
+    private void getUserInfo() {
+        firebaseFirestore.collection("UserInfo")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (document.getData().get("uid").toString().equals(user.getUid())) {
+                                    nickname.setText(document.getData().get("nickname").toString());
+                                    email.setText(document.getData().get("email").toString());
+                                    Uid = user.getUid();
+                                    Did = document.getId();
+                                }
+                            }
+                        } else {
+                            Nickname = "";
+                            Email = "";
+                            Uid = user.getUid();
+                            Did = "";
+                        }
+                    }
+                })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("Error !", e);
+            }
+        });
     }
 }
